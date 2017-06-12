3a0423d9a98c3f5927e23135460bf77d6247e0b5
giacomocusinato <giacomo.cusinato@outlook.com>
2017-06-12 19:49:53 +0200

    Complete base design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,39 +1,63 @@
 (function() {
 
-var bg = new Image();
+  var bg = new Image();
+  var current_photo = 0;
 
-$('section').hide();
+  $('header').fadeIn('slow').addClass('animated fadeIn');
+
+  $('nav ul li').each(function(i, item) {
+    $(item).delay(800 + i*600).fadeIn().addClass('animated slideInDown');
+  });
 
 
-$('.title').click(function() {
-  location.reload();
-});
+  $('.title').click(function() {
+    location.reload();
+  });
 
-$('.menu-item.about').click(function() {
-  loadBackground();
+  $('.menu-item.about').click(function() {
+    $('body').removeClass('bg-main').addClass('bg-white');
 
-  $('section').hide();
-  $('section.about').delay(400).fadeIn("slow");
-});
+    $('section').hide();
+    $('section.about').delay(400).fadeIn('slow');
+  });
 
-$('.menu-item.cv').click(function() {
-  loadBackground();
+  $('.menu-item.cv').click(function() {
+    $('body').removeClass('bg-main').addClass('bg-wall');
 
-  $('section').hide();
-  $('section.cv').delay(400).fadeIn("slow");
-});
+    $('section').hide();
+    $('section.cv').delay(400).fadeIn('slow');
+  });
 
-function loadBackground() {
-  if (!!bg.src) {
-    $("body").css("background-image", bg.src);
-    return;
+  $('.menu-item.photography').click(function() {
+    $('body').removeClass('bg-main').addClass('bg-white');
+
+    $('section').hide();
+    $('html').css('background', 'white');
+    $('section.photos').delay(400).fadeIn('slow');
+    $('.photos img').eq(current_photo).show();
+    // $('.photos img').each(function(i, el) {
+    //   $(el).delay(600 + 400 * i).fadeIn('slow');
+    // });
+
+  });
+
+  $('.photos img').click(function() {
+    $('.photos img').eq(current_photo).hide();
+    current_photo = current_photo + 1;
+    $('.photos img').eq(current_photo).show();
+  });
+
+
+  function loadBackground() {
+    if (!!bg.src) {
+      $("body").css("background-image", bg.src);
+      return;
+    }
+
+    bg.onload = function() {
+      $("body").css("background-image", "url(http://www.solidbackgrounds.com/images/2560x1440/2560x1440-white-solid-color-background.jpg)");
+    }
+    bg.src = "../images/background-wall.jpg";
   }
-
-  console.log('Loading..zx: '  + bg.src);
-  bg.onload = function(){
-    $("body").css("background-image", "url(../images/background-wall.jpg)");
-  }
-  bg.src = "../images/background-wall.jpg";
-}
 
 })();
