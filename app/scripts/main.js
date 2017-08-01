9ffa00023e1610b22cd97a35180c4f76afc1c5ba
giacomocusinato <giacomo.cusinato@outlook.com>
2017-08-01 15:33:46 +0200

    Remove gallery, modify code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,89 +22,18 @@
   });
 
 
-  $('.menu-item.about > a').click(function() {
-    toggleSubMenu(true);
+  $('.menu-item > a').click(function() {
+    var section = $(this).attr('data-open');
+
+    if (!section) return;
 
     $('.menu-item a').removeClass('active');
     $(this).addClass('active');
     $('body').removeClass('bg-main').addClass('bg-white');
 
     $('section').hide();
-    $('section.about').delay(400).fadeIn('slow');
+    $('section.' + section).delay(400).fadeIn('slow');
   });
-
-  $('.menu-item.cv > a').click(function() {
-    toggleSubMenu(true);
-
-    $('.menu-item a').removeClass('active');
-    $(this).addClass('active');
-    $('body').removeClass('bg-main').addClass('bg-white');
-
-    $('section').hide();
-    $('section.cv').delay(400).fadeIn('slow');
-  });
-
-  $('.menu-item.photography > a').click(function() {
-    toggleSubMenu();
-  });
-
-  $('.sub-menu-item a').click(function() {
-    $('.sub-menu-item a').removeClass('active');
-    $(this).addClass('active');
-
-    var category = $(this).attr('data-filter');
-
-    $('.photos').hide();
-    $('.photos.' + category).show();
-
-    if ($(window).width() > 768) {
-      $('.photos.' + category).fotorama({
-        width: 600,
-        ratio: '6/4',
-        minwidth: 200,
-        maxwidth: 600,
-        minheight: 300,
-        maxheight: 400,
-        allowfullscreen: true,
-        fit: 'contain',
-        nav: false,
-        transition: 'slide',
-        clicktransition: 'crossfade'
-      });
-    } else {
-      $('.photos.' + category).fotorama({
-        width: '100%',
-        ratio: '6/4',
-        allowfullscreen: true,
-        fit: 'contain',
-        nav: false,
-        transition: 'slide',
-        clicktransition: 'crossfade'
-      });
-    }
-
-    $('body').removeClass('bg-main').addClass('bg-white');
-    $('section').hide();
-    $('section.gallery').delay(400).fadeIn('slow');
-  });
-
-  function toggleSubMenu(close = false) {
-    var selector = '.menu-item.photography > a';
-
-    if ($(selector).hasClass('opened') || close) {
-      $('.menu-item a').removeClass('active');
-      $('.sub-menu-item').each(function(i, item) {
-        $(item).fadeOut();
-      });
-      $(selector).removeClass('opened');
-    } else {
-      $(selector).addClass('active');
-      $('.sub-menu-item').each(function(i, item) {
-        $(item).fadeIn().addClass('animated slideInDown');
-      });
-      $(selector).addClass('opened');
-    }
-  }
 
   function loadBackground() {
     if (!!bg.src) {
@@ -117,5 +46,70 @@
     };
     bg.src = '../images/background-wall.jpg';
   }
+
+
+  // OLD PHOTO GALLERY STUFF
+
+  // $('.menu-item.photography > a').click(function() {
+  //   toggleSubMenu();
+  // });
+  //
+  // $('.sub-menu-item a').click(function() {
+  //   $('.sub-menu-item a').removeClass('active');
+  //   $(this).addClass('active');
+  //
+  //   var category = $(this).attr('data-filter');
+  //
+  //   $('.photos').hide();
+  //   $('.photos.' + category).show();
+  //
+  //   if ($(window).width() > 768) {
+  //     $('.photos.' + category).fotorama({
+  //       width: 600,
+  //       ratio: '6/4',
+  //       minwidth: 200,
+  //       maxwidth: 600,
+  //       minheight: 300,
+  //       maxheight: 400,
+  //       allowfullscreen: true,
+  //       fit: 'contain',
+  //       nav: false,
+  //       transition: 'slide',
+  //       clicktransition: 'crossfade'
+  //     });
+  //   } else {
+  //     $('.photos.' + category).fotorama({
+  //       width: '100%',
+  //       ratio: '6/4',
+  //       allowfullscreen: true,
+  //       fit: 'contain',
+  //       nav: false,
+  //       transition: 'slide',
+  //       clicktransition: 'crossfade'
+  //     });
+  //   }
+  //
+  //   $('body').removeClass('bg-main').addClass('bg-white');
+  //   $('section').hide();
+  //   $('section.gallery').delay(400).fadeIn('slow');
+  // });
+  //
+  // function toggleSubMenu(close = false) {
+  //   var selector = '.menu-item.photography > a';
+  //
+  //   if ($(selector).hasClass('opened') || close) {
+  //     $('.menu-item a').removeClass('active');
+  //     $('.sub-menu-item').each(function(i, item) {
+  //       $(item).fadeOut();
+  //     });
+  //     $(selector).removeClass('opened');
+  //   } else {
+  //     $(selector).addClass('active');
+  //     $('.sub-menu-item').each(function(i, item) {
+  //       $(item).fadeIn().addClass('animated slideInDown');
+  //     });
+  //     $(selector).addClass('opened');
+  //   }
+  // }
 
 })();
